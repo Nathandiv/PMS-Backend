7b73e4d2a87a1f45b7ef5d8f0c1c694c4b9f23e8
Nathandiv <k.n.hlungwani@gmail.com>
2025-07-29 11:55:44 +0200

    added logic

--- src/main/java/pmsBackend/pmsBackend/repository/CohortRepository.java
+++ src/main/java/pmsBackend/pmsBackend/repository/CohortRepository.java
@@ -1,5 +1,10 @@
 package pmsBackend.pmsBackend.repository;
 
-public interface CohortRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pmsBackend.pmsBackend.entity.Cohort; // Import your Cohort entity
 
+@Repository
+public interface CohortRepository extends JpaRepository<Cohort, Long> {
+    // You can add custom query methods here if needed, e.g., findByCohortName
 }
--- src/main/java/pmsBackend/pmsBackend/repository/StudentRepository.java
+++ src/main/java/pmsBackend/pmsBackend/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package pmsBackend.pmsBackend.repository;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pmsBackend.pmsBackend.entity.Student; // Import your Student entity
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    // You can add custom query methods here if needed, e.g., findByEmailAddress, findByCohort
 }
